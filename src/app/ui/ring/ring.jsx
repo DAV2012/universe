27e3eaf504ad9d151bf7106ed3a1dc504b843088
Andres <andres.201219@gmail.com>
2024-06-19 14:08:52 -0500

    Orbitas

--- src/app/ui/ring/ring.jsx
+++ src/app/ui/ring/ring.jsx
@@ -1,12 +1,51 @@
 import styles from "@/app/ui/ring/ring.module.css"
+import { meteoras } from "@/app/lib/data"
+import Image from "next/image"
+
 
 
 export default function Ring(){
+    let distance = 200;
     return(
         <div className={styles.container}>
             <div className={styles.center}>
-                <div className={`${styles.ring} ${styles["ring-1"]}`}></div>
-                <div className={`${styles.ring} ${styles["ring-2"]}`}></div>
+                {meteoras.map((meteora, index)=>{
+                    const size = distance + (index * 200)
+                    let rotate = Math.random()*360
+                    return(
+                        <div 
+                        className={`${styles.ring}`} 
+                        key={meteora.id}
+                        style={{
+                            height: `${size}px`,
+                            width: `${size}px`,
+                            position: "absolute",
+                            top: "50%",
+                            left: "50%",
+                            transform: `translate(-50%, -50%) rotate(${Math.random()*360*2}deg)`
+                            
+                        }}
+                    >
+                        <Image
+                            src={meteora.src}
+                            alt="Celular iPhone-13"
+                            width={0}
+                            height={0}
+                            sizes="cover"
+                            style={{
+                                zIndex: 1,
+                                width: "auto",
+                                height: "75px",
+                                position:"absolute",
+                                left:"50%",
+                                transform: `translate(-50%, -50%)`
+                            }} // opcional, puede no ser necesario
+                        />
+                    </div>
+                    )
+                })}
+                
+                
     
 
             </div>
